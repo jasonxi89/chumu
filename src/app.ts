0a4912bc522bb5b009fd3b5e99897f9f06e36f9b
Xi Chen <jasonxi89@gmail.com>
2026-03-08 13:29:33 -0700

    fix: picker width overflow, add version log on launch

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import { PropsWithChildren } from 'react'
 import { useLaunch } from '@tarojs/taro'
 import { initCloud } from './utils/cloud'
+// @ts-ignore
+import { version } from '../package.json'
 import './app.scss'
 
 function App({ children }: PropsWithChildren) {
   useLaunch(() => {
+    console.log(`[初慕] v${version}`)
     initCloud()
   })
 
